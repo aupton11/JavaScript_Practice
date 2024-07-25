af77dac2fa537980d3234508dc6adc7c82a0986a
Annie Upton <farman1085.12@gmail.com>
2024-07-25 22:53:09 +0000

    Arrow

---
+++ Practice/Arrow_Function.js
@@ -0,0 +1,14 @@
+//      Arrow Function
+// let myArrow=()=>{
+//     let myLove="John";
+//     console.log(myLove);
+// }
+
+// myArrow();
+
+// let myArrow=()=>"hello";
+// console.log(myArrow());
+
+
+// let myArrow=a=>a;
+// console.log(myArrow("john"));
